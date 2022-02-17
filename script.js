dde1baf6a2168a8458defccec2a353606b035270
federico1018 <faldana4@misena.edu.co>
2022-02-17 17:16:18 -0500

    Proyecto inicilizado_TemplateBasica

---
+++ script.js
@@ -0,0 +1,21 @@
+/*Use Vanilla.Js para lograr la interración con el Menu Mobile.*/
+
+/* ********** Menu_Mobile ********** */
+((d) => {
+  const $btnMenu = d.querySelector(".menu-btn"),
+    $menu = d.querySelector(".menu");
+
+  $btnMenu.addEventListener("click", (e) => {
+    $btnMenu.firstElementChild.classList.toggle("none");
+    $btnMenu.lastElementChild.classList.toggle("none");
+    $menu.classList.toggle("is-active");
+  });
+
+  d.addEventListener("click", (e) => {
+    if (!e.target.matches(".menu__enlace")) return false;
+
+    $btnMenu.firstElementChild.classList.remove("none");
+    $btnMenu.lastElementChild.classList.add("none");
+    $menu.classList.remove("is-active");
+  });
+})(document);
