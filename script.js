baddfeddecef99fa0bb76378ffcac132ddff8777
federico1018 <faldana4@misena.edu.co>
2022-02-18 01:46:49 -0500

    ContadorV.1 Mejorar diseño

--- script.js
+++ script.js
@@ -26,3 +26,22 @@ $dark.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   $dark.classList.toggle("active");
 });
+
+/*Script CONTADOR*/
+let contador = 0;
+const valor = document.querySelector("#valor");
+const botones = document.querySelectorAll(".boton");
+botones.forEach((boton) => {
+  boton.addEventListener("click", function (e) {
+    const estilos = e.currentTarget.classList;
+
+    if (estilos.contains("disminuir")) {
+      contador--;
+    } else if (estilos.contains("aumentar")) {
+      contador++;
+    } else {
+      contador = 0;
+    }
+    valor.textContent = contador;
+  });
+});
